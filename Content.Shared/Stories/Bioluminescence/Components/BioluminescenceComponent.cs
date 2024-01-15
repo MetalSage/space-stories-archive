330387017c689de4caa6966fdcb1875cf322d875
Fragoler <e.f.oleg@mail.ru>
2024-01-15 18:31:19 +0300

    Fixed some invailid ent on Corvax maps
    Add Dressers to maps:
    - Aspid
    - Atlas
    - Avrite
    - Bagel
    - barratry
    - box
    - cluster
    - core
    - delta
    - fland
    - gate
    - silly
    - outpost
    - spectrum

--- Content.Shared/Stories/Bioluminescence/Components/BioluminescenceComponent.cs
+++ Content.Shared/Stories/Bioluminescence/Components/BioluminescenceComponent.cs
@@ -1,7 +1,10 @@
 using Content.Shared.Actions;
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.SpaceStories.Bioluminescence;
 
 [RegisterComponent]
+[NetworkedComponent]
 public sealed partial class BioluminescenceComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField("action")]
