9982f753574d8e6f848c82a785467434105c1f93
Fragoler <e.f.oleg@mail.ru>
2024-02-01 20:37:07 +0300

    Use popupClient

--- Content.Server/Stories/Implants/InjectImplantSystem.cs
+++ Content.Server/Stories/Implants/InjectImplantSystem.cs
@@ -1,44 +1,12 @@
 using Content.Server.Administration.Logs;
-using Content.Server.Body.Systems;
 using Content.Server.Chemistry.Containers.EntitySystems;
-using Content.Server.Explosion.Components;
-using Content.Server.Flash;
-using Content.Server.Flash.Components;
-using Content.Server.Radio.EntitySystems;
 using Content.Shared.Chemistry.Components;
-using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Database;
 using Content.Shared.Implants.Components;
-using Content.Shared.Interaction;
-using Content.Shared.Mobs;
-using Content.Shared.Mobs.Components;
-using Content.Shared.Payload.Components;
-using Content.Shared.Radio;
-using Content.Shared.Slippery;
-using Content.Shared.StepTrigger.Systems;
-using Content.Shared.Trigger;
 using Content.Shared.FixedPoint;
-using Content.Shared.Weapons.Ranged.Events;
-using JetBrains.Annotations;
-using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
-using Robust.Shared.Containers;
-using Robust.Shared.Physics.Events;
-using Robust.Shared.Physics.Systems;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Random;
-using Content.Server.Administration.Logs;
-using Content.Server.Body.Systems;
-using Content.Server.Chemistry.Containers.EntitySystems;
-using Content.Server.Interaction;
-using Content.Server.Popups;
 using Content.Shared.Chemistry;
-using Content.Shared.CombatMode;
-using Content.Shared.DoAfter;
-using Content.Shared.Mobs.Systems;
-using Robust.Shared.Audio.Systems;
 using Content.Shared.IdentityManagement;
-
 using Content.Server.Explosion.EntitySystems;
 using Content.Shared.Chemistry.Reagent;
 using Content.Shared.Popups;
@@ -79,7 +47,7 @@ namespace Content.Server.Stories.Implants
             if (!component.NotUsedSolutions.ContainsValue(true))
             {
                 // Have no capsules
-                _popup.PopupEntity(Loc.GetString("inject-trigger-empty-message"), user, user);
+                _popup.PopupClient(Loc.GetString("inject-trigger-empty-message"), user, user);
                 return;
             }
 
@@ -97,7 +65,7 @@ namespace Content.Server.Stories.Implants
             // Try get insert solution
             if (!_solutionContainer.TryGetInjectableSolution(user, out var targetSoln, out var targetSolution))
             {
-                _popup.PopupEntity(Loc.GetString("inject-trigger-cant-inject-message", ("target", Identity.Entity(user, _entMan))), user, user);
+                _popup.PopupClient(Loc.GetString("inject-trigger-cant-inject-message", ("target", Identity.Entity(user, _entMan))), user, user);
                 return;
             }
 
@@ -109,7 +77,7 @@ namespace Content.Server.Stories.Implants
 
             if (realTransferAmount <= 0)
             {
-                _popup.PopupEntity(Loc.GetString("inject-trigger-empty-capsule-message"), user, user);
+                _popup.PopupClient(Loc.GetString("inject-trigger-empty-capsule-message"), user, user);
                 return;
             }
 
@@ -118,14 +86,14 @@ namespace Content.Server.Stories.Implants
 
             if (!targetSolution.CanAddSolution(removedSolution))
             {
-                _popup.PopupEntity(Loc.GetString("inject-trigger-cant-inject-message", ("target", Identity.Entity(user, _entMan))), user, user);
+                _popup.PopupClient(Loc.GetString("inject-trigger-cant-inject-message", ("target", Identity.Entity(user, _entMan))), user, user);
                 return;
             }
 
             _reactiveSystem.DoEntityReaction(user, removedSolution, ReactionMethod.Injection);
             _solutionContainers.TryAddSolution(targetSoln.Value, removedSolution);
 
-            _popup.PopupEntity(Loc.GetString("inject-trigger-feel-prick-message"), user, user);
+            _popup.PopupClient(Loc.GetString("inject-trigger-feel-prick-message"), user, user);
 
             // same LogType as syringes...
             _adminLogger.Add(LogType.ForceFeed, $"{_entMan.ToPrettyString(user):user} used inject implant with a solution {SolutionContainerSystem.ToPrettyString(removedSolution):removedSolution}");
